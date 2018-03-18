0a4b715758f96fc366643e5fe1277a1e24db8775
Cheejyg <glenncheejunyuan@hotmail.com>
2018-03-18 10:02:20 +0800

    SAfindAr

---
+++ Section A/SAfindAr/SAfindAr/SAfindAr.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int findAr(int size, int ar[], int target);
+void main() {
+	int ar[20];
+	int size, i, target;
+
+	printf("Enter array size: ");
+	scanf("%d", &size);
+	printf("Enter %d data: ", size);
+	for (i = 0; i < size; i++) {
+		scanf("%d", &ar[i]);
+	}
+	printf("Enter the target number: ");
+	scanf("%d", &target);
+	printf("findAr(): %d", findAr(size, ar, target));
+
+}
+int findAr(int size, int ar[], int target) {
+	/* write your code here */
+	int x;
+	int t = -1;
+
+	for (x = 0; x < size; x++) {
+		if (ar[x] == target) {
+			t = x;
+			break;
+		}
+	}
+
+	return t;
+}
